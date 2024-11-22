40c0eefdfc60fa552db9715fe9f33c2fc94a03b8
sfaber34 <sfaber34@gmail.com>
2024-11-22 12:06:42 -0700

    added block number and hash for 10000 back

--- index.js
+++ index.js
@@ -18,13 +18,32 @@ async function getBlockHash(blockNumber) {
 }
 
 const currentBlockNumber = await getCurrentBlockNumber();
-const blockHash = await getBlockHash(currentBlockNumber);
 
-console.log(`Current Block Number:\t${currentBlockNumber}`);
-console.log(`Current Block Hash:\t${blockHash}`);
+try {
+  const blockHash = await getBlockHash(currentBlockNumber);
 
-const blockNumberBack10000 = currentBlockNumber - BigInt(10000);
-const blockHashBack10000 = await getBlockHash(blockNumberBack10000);
+  console.log(`Current Block Number:\t${currentBlockNumber}`);
+  console.log(`Current Block Hash:\t${blockHash}`);
+} catch (error) {
+  console.error(`Current Block Error:\t${error}`);
+}
 
-console.log(`Block Number Back 10000:\t${blockNumberBack10000}`);
-console.log(`Block Hash Back 10000:\t${blockHashBack10000}`);
+try {
+  const blockNumberBack10000 = currentBlockNumber - BigInt(10000);
+  const blockHashBack10000 = await getBlockHash(blockNumberBack10000);
+
+  console.log(`Block Number Back 10000:\t${blockNumberBack10000}`);
+  console.log(`Block Hash Back 10000:\t${blockHashBack10000}`);
+} catch (error) {
+  console.error(`Back 10000 Block Error:\t${error}`);
+}
+
+try {
+  const blockNumberBack20000 = currentBlockNumber - BigInt(20000);
+  const blockHashBack20000 = await getBlockHash(blockNumberBack20000);
+
+  console.log(`Block Number Back 20000:\t${blockNumberBack20000}`);
+  console.log(`Block Hash Back 20000:\t${blockHashBack20000}`);
+} catch (error) {
+  console.error(`Back 20000 Block Error:\t${error}`);
+}
